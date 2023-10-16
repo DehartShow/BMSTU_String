aa8549e785037cd6e552a60e8913b529a4fdd2d3
DehartShow <voznyukdima02032005@gmail.com>
2023-10-16 09:21:03 +0300

    first commit

---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by dehart on 10/16/23.
+//
+
+#ifndef CMAKE_EXAMPLE_REPO_MAIN_H
+#define CMAKE_EXAMPLE_REPO_MAIN_H
+
+#endif //CMAKE_EXAMPLE_REPO_MAIN_H
--- test.cpp
+++ test.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 #include <string>
+#include "main.h"
+
 TEST(GoogleTestCi, Foo) {
   ASSERT_TRUE(true);
 }
